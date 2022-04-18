5591cf89f770eb841ecf04561b7558ca15ccdd32
dewomser <webnaster@untergang.de>
2022-04-18 21:57:32 +0200

    a personalisierung entfernt

--- get-rss1.sh
+++ get-rss1.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd /home/karl/bin/wz-test || exit
+cd $HOME/bin/wz-test || exit
 
 wget --quiet -O "$1".txt "$2"
 
 ./parse1.sh "$1".txt > "$1".html
 
-/usr/bin/dillo "$1".html
+#Hier Browser einstellen
+#/usr/bin/dillo "$1".html
+#oder default Browser
+xdg-open "$1".html
